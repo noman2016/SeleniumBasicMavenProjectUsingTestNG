5697e6a6881a308bfce638a0cbb27bf9ee15a296
noman2016 <hafezalnoman@gmail.com>
2022-03-20 16:41:50 +0600

    Add SkipTest File

---
+++ src/test/java/ui/SkipTest.java
@@ -0,0 +1,32 @@
+package ui;
+
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+public class SkipTest {
+
+	Boolean datasetup = true;
+
+	@Test(enabled = false)
+	public void skipTest1() {
+		System.out.println("skipping this test as it is not complete");
+	}
+	
+	@Test
+	public void skipTest2() {
+		System.out.println("skipping this test forcefully");
+		throw new SkipException("Skiping this test");
+	}
+	
+	@Test
+	public void skipTest3() {
+
+		System.out.println("skipping this test based on condition");
+		if (datasetup == true) {
+			System.out.println("Execute the test");
+		} else {
+			System.out.println("Do not execute further steps");
+			throw new SkipException("Do not execute further steps");
+		}
+	}
+}
